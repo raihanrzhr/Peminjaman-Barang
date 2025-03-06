14f9fddb2c3df4555b1395c2c9c89f93b8698653
Raihan Nurazhar <raihannurazhar@upi.edu>
2025-03-06 11:46:57 +0700

    mengubah tampilan items.blade

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -1,13 +1,22 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    @foreach ($items as $item)
-        <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $item['namaBarang'] }}</h2>
-            <div class="text-base text-gray-600">
-                <a href="">{{ $item['peminjam'] }}</a> | 1 januari 2025
-            </div>
-            <p class="my-4 font-light">{{ $item['deskripsi'] }}</p>
-        </article>
-    @endforeach
+    <table class="min-w-full border-collapse border border-gray-300">
+        <thead>
+            <tr>
+                <th class="border border-gray-300 px-4 py-2">ID</th>
+                <th class="border border-gray-300 px-4 py-2">Nama Barang</th>
+                <th class="border border-gray-300 px-4 py-2">Spesifikasi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($items as $item)
+                <tr>
+                    <td class="border border-gray-300 px-4 py-2">{{ $item['id'] }}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{ $item['namaBarang'] }}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{ $item['spesifikasi'] }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-layout>

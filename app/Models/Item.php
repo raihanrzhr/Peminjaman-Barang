2a96eed2d619175205fff764d6ab52a1103c6a0c
Raihan Nurazhar <raihannurazhar@upi.edu>
2025-03-07 11:10:51 +0700

    memperbaiki fungsi model

---
+++ app/Models/Item.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Item
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 'PR001',
+                'namaBarang' => 'Proyektor',
+                'spesifikasi' => 'Proyektor Epson EB-X05 memiliki tingkat kecerahan 3.300 lumens dengan resolusi XGA 1024 x 768 piksel. Proyektor ini memiliki kontras 15.000:1 dan teknologi 3LCD yang menghasilkan warna yang lebih cerah dan tajam. Proyektor ini juga memiliki fitur kecerahan warna 3.300 lumens dan kecerahan putih 3.300 lumens.'
+            ],
+            [
+                'id' => 'LP001',
+                'namaBarang' => 'Laptop',
+                'spesifikasi' => 'Laptop ASUS VivoBook A412DA merupakan laptop yang memiliki desain yang stylish dan ringan. Laptop ini memiliki layar NanoEdge 14 inci dengan bezel tipis yang membuat tampilan layar lebih luas. Laptop ini juga memiliki prosesor AMD Ryzen 5 3500U dan RAM 8GB DDR4.'
+            ]
+        ];  
+    }
+
+    public static function find($slug): array
+    {
+        $item = Arr::first(static::all(), fn ($item) => $item['slug'] == $slug);
+
+        if (!$item) {
+            abort(404);
+        }
+
+        return $item;
+    }  
+}
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,25 @@
 <?php
 
+use App\Models\Item;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+
+
 
 Route::get('/', function () {
     return view('peminjaman', ['title' => 'Tabel Peminjaman']);
 });
 
 Route::get('/items', function () {
-    return view('items', ['title' => 'Tabel Barang', 'items' =>[
-        [
-            'id' => 'PR001',
-            'namaBarang' => 'Proyektor',
-            'spesifikasi' => 'Proyektor Epson EB-X05 memiliki tingkat kecerahan 3.300 lumens dengan resolusi XGA 1024 x 768 piksel. Proyektor ini memiliki kontras 15.000:1 dan teknologi 3LCD yang menghasilkan warna yang lebih cerah dan tajam. Proyektor ini juga memiliki fitur kecerahan warna 3.300 lumens dan kecerahan putih 3.300 lumens.'
-        ],
-        [
-            'id' => 'LP001',
-            'namaBarang' => 'Laptop',
-            'spesifikasi' => 'Laptop ASUS VivoBook A412DA merupakan laptop yang memiliki desain yang stylish dan ringan. Laptop ini memiliki layar NanoEdge 14 inci dengan bezel tipis yang membuat tampilan layar lebih luas. Laptop ini juga memiliki prosesor AMD Ryzen 5 3500U dan RAM 8GB DDR4.'
-        ]
-    ]]);
+    return view('items', ['title' => 'Tabel Barang', 'items' => Item::all()]);
+});
+
+Route::get('/items/{slug}', function ($slug) {
+    
+    $items = Item::find($slug);
+
+    return view('item', ['title' => 'Detail Barang', 'item' => $items]);
 });
 
 Route::get('/borrowers', function () {

f3ff51094fa37825cdb21e1c7c298b9308c2df66
Raihan Nurazhar <raihannurazhar@upi.edu>
2025-03-10 11:27:13 +0700

    menambahkan migrasi dan koneski database

---
+++ database/migrations/2025_03_10_041326_create_peminjaman_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('id_barang', 10);
+            $table->string('id_peminjam', 10);
+            $table->date('tanggal_pinjam')->default(DB::raw('curdate()'));
+            $table->date('tanggal_kembali')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
